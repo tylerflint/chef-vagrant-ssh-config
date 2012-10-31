a1d126ff3d5b055290f4ae1991b25bafb1d24d05
Tyler Flint <tylerflint@gmail.com>
2012-10-30 22:10:41 -0600

    adding authorized_keys and bumping version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "tylerflint@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures vagrant-ssh-config"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.2.0"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -13,6 +13,15 @@ if ::File.exists? '/tmp/.ssh'
       owner user
       mode "0755"
     end
+    
+    file "#{home}/.ssh/authorized_keys" do
+      owner user
+      mode "0600"
+    end
+
+    execute "cat /tmp/.ssh/id_rsa.pub >> #{home}/.ssh/authorized_keys" do
+      not_if { `cat #{home}/.ssh/authorized_keys | grep "$(cat /tmp/.ssh/id_rsa.pub)" | wc -l`.to_i != 0 }
+    end
 
     cookbook_file "#{home}/.ssh/config" do
       owner user
